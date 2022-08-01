4a2a5a51d1753c7d1be23c70dcb5fac1152fda1d
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-08-01 09:08:52 -0700

    added a basic outline for orders

---
+++ src/main/java/com/sportyshoes/model/Order.java
@@ -0,0 +1,13 @@
+package com.sportyshoes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+
+}
